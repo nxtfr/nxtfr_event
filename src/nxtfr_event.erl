994105dd1ebc78a2976844cf9ba33c783212db3f
Cflow <christian@flodihn.se>
2021-10-21 08:32:48 +0200

    Automatically created event manager when adding global handler.

--- src/nxtfr_event.erl
+++ src/nxtfr_event.erl
@@ -14,17 +14,25 @@
 add_handler(Handler) ->
     gen_event:add_sup_handler(nxtfr_event, Handler, []).
 
-add_global_handler(EventHandler, Handler) ->
-    gen_event:add_sup_handler({global, EventHandler}, Handler, []).
+add_global_handler(EventManager, Handler) ->
+    case global:whereis_name(EventManager) of
+        undefined ->
+            nxtfr_global_event_sup:start(EventManager);
+        _Pid ->
+            pass
+    end,
+    gen_event:add_sup_handler({global, EventManager}, Handler, []).
 
 notify(Event) ->
     gen_event:notify(nxtfr_event, Event).
 
 notify_global(EventManager, Event) ->
+    global:whereis_name(EventManager) =/= undefined orelse throw(no_event_manager),
     gen_event:notify({global, EventManager}, Event).
 
 delete_handler(Handler) ->
     gen_event:delete_handler(nxtfr_event, Handler).
 
 delete_global_handler(EventManager, Handler) ->
+    global:whereis_name(EventManager) =/= undefined orelse throw(no_event_manager),
     gen_event:delete_handler({global, EventManager}, Handler).
